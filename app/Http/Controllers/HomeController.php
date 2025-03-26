e018549a77bcd451d29f3eee829c2cd5705a1fbb
omartins-zs <gabrielmatheus209@gmail.com>
2025-03-25 23:16:45 -0300

    Criando Function Index para Chamar a View

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    /**
+     * Exibe a view com o componente Vue para envio dos arquivos.
+     */
+    public function index()
+    {
+        return view('welcome');
+    }
 }
